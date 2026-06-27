4bb70429a79a29f6d4161451b13f8667b2552e86
Apriyudha <apriyudha307@gmail.com>
2026-06-27 10:34:26 +0700

    edit model studio

--- models/studio.go
+++ models/studio.go
@@ -12,7 +12,7 @@ type Studio struct {
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
-
+	Tipe        string         `gorm:"column:tipe;size:20;default:Reguler"`
 	NamaStudio  string `gorm:"column:nama_studio"`
 	JumlahBaris int    `gorm:"column:jumlah_baris"`
 	JumlahKolom int    `gorm:"column:jumlah_kolom"`
